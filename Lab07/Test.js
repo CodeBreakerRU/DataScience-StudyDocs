74d7836ef4345a834a638d1e9e085d9892a31e6f
cs-riley <liyanaarachchirasika@gmail.com>
2019-10-03 23:45:40 +0530

    Lab 07

---
+++ Lab07/Test.js
@@ -0,0 +1,13 @@
+db.DBBooks.find({year:2005})
+db.DBBooks.find()
+db.DBBooks.count()
+
+db.DBBooks.count({year:2005})
+
+db.DBBooks.find({year:2005, price:{$gt:250}})
+u
+db.DBBooks.find({year:2005, price:{$lt:250}})
+
+db.DBBooks.find({price:{$gt:150}})
+
+db.DBBooks.find({year:{$exists:false}})
